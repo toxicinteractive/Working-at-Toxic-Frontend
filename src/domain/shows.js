b6e9390197c49dfe1b45633776d0953a297a559d
Christian Yngvesson <christian.yngvesson@toxic.se>
2022-11-08 10:13:40 +0100

    Get show by Id API call

--- src/domain/shows.js
+++ src/domain/shows.js
@@ -4,9 +4,15 @@ import get from "./fetchData";
 const key = '7aa3b5d1a41f62bb96b4ce0b8666cf57';
 
 export async function getByPopularity() {
-    const url = `https://api.themoviedb.org/3/tv/popular?api_key=${key}&language=en-US&page=1`
-    const result = await get(url)
+    const url = `https://api.themoviedb.org/3/tv/popular?api_key=${key}&language=en-US&page=1`;
+    const result = await get(url);
     return result;
+}
+
+export async function getShowById(id) {
+  const url = `https://api.themoviedb.org/3/tv/${id}?api_key=${key}&language=en-US`;
+  const result = await get(url);
+  return result;
 }
 
 export default getByPopularity;
