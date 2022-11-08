3450229da7ca6c6a7c543336435940453b284649
Christian Yngvesson <christian.yngvesson@toxic.se>
2022-11-08 16:30:22 +0100

    Refactored rendering conditions of Details

--- src/components/ShowDetails.jsx
+++ src/components/ShowDetails.jsx
@@ -22,28 +22,30 @@ export default class ShowDetails extends React.Component {
     }
 
     render() {
-        return (
-            <>
-                <div className='show-details'>
-                    <h2>{this.state.show ? this.state.show.name : <></>}</h2>
-                    <h3>{this.state.show ? this.state.show.tagline : <></>}</h3>
-                    <div className='show-details-summary'>
-                        <div className='show-details-info'>
-                            <h5>Genre:</h5> {this.state.show ? <List list={this.state.show.genres} property='name' /> : <></>}
-                            <h5>Languages:</h5> {this.state.show ? <List list={this.state.show.spoken_languages} property='english_name' /> : <></>}
-                            <h5>Seasons:</h5> {this.state.show ? this.state.show.number_of_seasons : <></>}
-                            <h5>Episodes:</h5> {this.state.show ? this.state.show.number_of_episodes : <></>}
-                            <h5>Networks:</h5> {this.state.show ? <List list={this.state.show.networks} property='name' /> : <></>}
+        if (this.state.show)
+            return (
+                <>
+                    <div className='show-details'>
+
+                        <h2>{this.state.show.name}</h2>
+                        <h3>{this.state.show.tagline}</h3>
+                        <div className='show-details-summary'>
+                            <div className='show-details-info'>
+                                <h5>Genre:</h5> <List list={this.state.show.genres} property='name' />
+                                <h5>Languages:</h5> <List list={this.state.show.spoken_languages} property='english_name' />
+                                <h5>Seasons:</h5> {this.state.show.number_of_seasons}
+                                <h5>Episodes:</h5> {this.state.show.number_of_episodes}
+                                <h5>Networks:</h5> <List list={this.state.show.networks} property='name' />
+                            </div>
+                            <img src={this.imagePath()} className='show-details-thumbnail'></img>
                         </div>
-                        <img src={this.imagePath()} className='show-details-thumbnail'></img>
+                        <div className='show-details-overview'>{this.state.show.overview}</div>
                     </div>
-                    <div className='show-details-overview'>{this.state.show ? this.state.show.overview : <></>}</div>
-                </div>
 
-                <button className='show-details-back-button' onClick={this.props.returnFunction}>Back to TV Shows</button>
+                    <button className='show-details-back-button' onClick={this.props.returnFunction}>Back to TV Shows</button>
 
-                <div className='void'></div>
-            </>
-        )
+                    <div className='void'></div>
+                </>
+            )
     }
-}
+} 
