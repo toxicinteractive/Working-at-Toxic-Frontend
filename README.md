cbe2b35065da69c2a715a3e11acf971a8d27f58c
Emil Karlsson <emil.h.karlsson@gmail.com>
2022-11-10 12:51:44 +0100

    JSON parsed and can display data

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ When the user clicks on a show, a new page with detailed information about the s
 
 When you're done, preferably within a week of seeing this, send us a pull request with your work in this repo! Bonus points if your commitments are descriptive, and what future development of the project could be.
 
+## Notes from me
+I have a clear view of how the complete website should look and how to structure the tv series.
+
+Although, I am Struggling with extracting data from JSON, haven't touched .json, APIs etc since I studied at my last
+program in 2019.
+
--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
 const app = express()
+const fs = require('fs');
+
+//JSON
+let rawdata = fs.readFileSync('data.json')
+let parsedData = JSON.parse(rawdata)
+let tvSeries = parsedData.results
 
 app.engine("hbs", expressHandlebars.engine({
     defaultLayout: 'main.hbs'
-  }))
+}))
+app.use(express.static('public'))
 
-app.get('/', function(request, response){
-    response.render('index.hbs')
-    
+app.get('/', function (request, response) {
+    const model = {
+        series: tvSeries
+    }
+    response.render('index.hbs', model)
 })
 
-
-
-function getData(){
-    var dataString = JSON.parse(data)
-console.log(dataString)
-}
-
 app.listen(8080)
+console.log('Listening to port 8080')
