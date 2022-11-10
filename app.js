aa04cdbe267ba3d1c208dc7cb790ee5dcd6c722d
Emil Karlsson <emil.h.karlsson@gmail.com>
2022-11-10 14:01:36 +0100

    Started working on individual series page

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const fs = require('fs');
 let rawdata = fs.readFileSync('data.json')
 let parsedData = JSON.parse(rawdata)
 let tvSeries = parsedData.results
+//sorting by latest airdate
+tvSeries.sort((a,b) =>  new Date(b.first_air_date) - new Date(a.first_air_date));
+
 
 app.engine("hbs", expressHandlebars.engine({
     defaultLayout: 'main.hbs'
@@ -18,6 +21,18 @@ app.get('/', function (request, response) {
         series: tvSeries
     }
     response.render('index.hbs', model)
+})
+app.get('/individual_series/:id', function(request,response){
+    const clickedSeriesId = request.params.id
+    const clickedSeries = tvSeries.find(series=>{
+        return series.id == clickedSeriesId
+    })
+    console.log('Clicked Series :)',clickedSeries)
+    const model ={
+        series: clickedSeries
+    }
+    response.render('individual_series.hbs', model)
+
 })
 
 app.listen(8080)
