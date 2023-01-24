4f409655faa65383be26a3fcf71c7ea9175015c5
Kevin-Krantz <kevinkrantz95@hotmail.com>
2023-01-24 13:25:27 +0100

    Created necessary types for the project

--- tmdb-api/src/store/types/Api.ts
+++ tmdb-api/src/store/types/Api.ts
@@ -6,3 +6,100 @@ export interface IApiCallBeganPayload {
   onSuccess?: string;
   onFailed?: string;
 }
+
+export interface IMovie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface IMovieData {
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface IApiResponse {
+  status: string;
+  endpointName: string;
+  requestId: string;
+  startedTimeStamp: number;
+  data: IMovieData;
+  fulfilledTimeStamp?: number;
+  isError?: boolean;
+  isFetching?: boolean;
+  isLoading?: boolean;
+  isSuccess?: boolean;
+  isUninitialized?: boolean;
+  refetch?: Function;
+}
+
+export interface IApiResponseSingle {
+  data: IMovieSingle;
+  status: string;
+  isUninitialized: boolean;
+  isLoading: boolean;
+  isSuccess: boolean;
+  isError: boolean;
+  isFetching: boolean;
+}
+
+export interface IMovieSingle {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: {
+    id: number;
+    name: string;
+    poster_path: string;
+    backdrop_path: string;
+  };
+  budget: number;
+  genres: {
+    id: number;
+    name: string;
+  }[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: {
+    id: number;
+    logo_path: string;
+    name: string;
+    origin_country: string;
+  }[];
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
