5d7231277628bf8526cc3f6726e45ffb4a8f1d12
Kevin-Krantz <kevinkrantz95@hotmail.com>
2023-01-24 15:00:04 +0100

    Added language and sortBy reducers to the CombineReducers

--- tmdb-api/src/store/entities.ts
+++ tmdb-api/src/store/entities.ts
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
+import languageReducer from "./language";
 import searchReducer from "./search";
+import sortMovieReducer from "./sortMovie";
 
 export default combineReducers({
   search: searchReducer,
+  language: languageReducer,
+  sortBy: sortMovieReducer,
 });
