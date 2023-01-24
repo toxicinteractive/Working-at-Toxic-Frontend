9d4607f3937b8dad6f96056ab5dde46055135a0a
Kevin-Krantz <kevinkrantz95@hotmail.com>
2023-01-24 14:38:53 +0100

    Combined all the reducers into a single root reducer

--- tmdb-api/src/store/reducer.ts
+++ tmdb-api/src/store/reducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+import entitiesReducer from "./entities";
 import api from "./rtkApi";
 
 export default combineReducers({
+  entities: entitiesReducer,
   [api.reducerPath]: api.reducer,
 });
